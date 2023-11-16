caa491075e8ba0b1ce942349e37de949c8b545af
asadali <asad.ali@builtinsoft.com>
2023-11-16 08:10:41 +0500

    update

--- src/helpers/constant.ts
+++ src/helpers/constant.ts
@@ -1,2 +1,2 @@
 export const MONGODB_URI = "mongodb+srv://asadalicodding:GIsAPQj4Y6eTEwSO@cluster0.aioacy2.mongodb.net/?retryWrites=true&w=majority"
-export const listenPort=3000
+export const listenPort=80
